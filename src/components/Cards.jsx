1cd70e07f37a3145dbe34208614ad6e413da0cab
Matheus Mattos Avelino <matheusma2510@hotmail.com>
2023-04-04 22:40:21 +0000

    server create

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -5,23 +5,37 @@ import {
   ImageBackground,
   TouchableOpacity,
 } from "react-native";
-import React from "react";
-
-import { cards } from "../data";
+import React, { useEffect, useState } from "react";
 import { LinearGradient } from "expo-linear-gradient";
+import api from "../api";
 
 const Cards = () => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  function fetchAPI() {
+    api
+      .get("/foods")
+      .then((res) => setData(res.data))
+      .catch((error) => console.error(error));
+  }
+
+  useEffect(() => {
+    fetchAPI();
+  }, []);
+
+  console.log(data);
   return (
     <FlatList
       className="w-full py-4"
-      data={cards}
+      data={data}
       renderItem={({ item }) => (
         <TouchableOpacity
           className="w-full rounded-xl mb-4 h-[172px] overflow-hidden"
           activeOpacity={0.8}
         >
           <ImageBackground
-            source={{ uri: item.img }}
+            source={{ uri: item.cover }}
             resizeMode="cover"
             className="flex-1"
           >
@@ -31,17 +45,18 @@ const Cards = () => {
               locations={[0.2, 1]}
             >
               <Text className="font-interSemi text-xl text-white mb-2">
-                {item.title}
+                {item.name}
               </Text>
               <View className="flex-row">
-                <Text className="mr-4 text-white">{item.info.ingredients}</Text>
-                <Text className="text-white">{item.info.time}</Text>
+                <Text className="text-white">
+                  {item.total_ingredients} | {item.time}
+                </Text>
               </View>
             </LinearGradient>
           </ImageBackground>
         </TouchableOpacity>
       )}
-      keyExtractor={(item) => item.id}
+      keyExtractor={(item) => String(item.id)}
     />
   );
 };
